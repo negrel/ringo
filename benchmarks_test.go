751c3f3afce5c4470d5442a45adb9f47106327a5
Alexandre Negrel <alexandre@negrel.dev>
2024-03-18 16:58:15 +0100

    update benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -19,30 +19,6 @@ func BenchmarkRing(b *testing.B) {
 	}
 }
 
-// func BenchmarkOneToOne(b *testing.B) {
-// 	buffer := NewOneToOne(uint32(b.N))
-//
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-//
-// 	b.ResetTimer()
-//
-// 	for i := 0; i < b.N; i++ {
-// 		y := i
-// 		buffer.Push(Generic(&y))
-// 	}
-//
-// 	go func() {
-// 		defer wg.Done()
-// 		for i := 0; i < b.N; i++ {
-// 			data, _ := buffer.Shift()
-// 			result = data
-// 		}
-// 	}()
-//
-// 	wg.Wait()
-// }
-
 func max(x, y int) int {
 	if x < y {
 		return y
@@ -90,48 +66,44 @@ func BenchmarkManyToOne(b *testing.B) {
 	}
 }
 
-// func BenchmarkManyToMany(b *testing.B) {
-// 	buffer := ManyToMany(uint32(b.N))
-// 	// Number of concurrent writer
-// 	rwCount := 100
-//
-// 	var wg sync.WaitGroup
-// 	wg.Add(rwCount)
-//
-// 	// Avoid b.N / rwCount = 0
-// 	loop := float64(b.N)
-// 	if loop == 1 {
-// 		loop = float64(rwCount)
-// 	}
-// 	loop /= float64(rwCount)
-// 	loopPerGoroutine := int(math.Ceil(loop))
-//
-// 	writer := func() {
-// 		defer wg.Done()
-// 		for i := 0; i < loopPerGoroutine; i++ {
-// 			j := i
-// 			buffer.Push(Generic(&j))
-// 		}
-// 	}
-//
-// 	reader := func() {
-// 		defer wg.Done()
-// 		for i := 0; i < loopPerGoroutine; i++ {
-// 			value, _ := buffer.Shift()
-// 			_ = value
-// 		}
-// 	}
-//
-// 	b.ResetTimer()
-// 	for i := 0; i < rwCount; i++ {
-// 		go writer()
-// 	}
-//
-// 	wg.Wait()
-// 	wg.Add(rwCount)
-//
-// 	for i := 0; i < rwCount; i++ {
-// 		go reader()
-// 	}
-// 	wg.Wait()
-// }
+func BenchmarkManyToOneWaiter(b *testing.B) {
+	buffer := NewWaiter(NewManyToOne[int](b.N))
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < b.N; i++ {
+			buffer.Push(i)
+		}
+	}()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buffer.Next()
+	}
+}
+
+func BenchmarkManyToOnePoller(b *testing.B) {
+	buffer := NewPoller(NewManyToOne[int](b.N))
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < b.N; i++ {
+			buffer.Push(i)
+		}
+	}()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buffer.Next()
+	}
+}
