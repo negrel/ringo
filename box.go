68072abf83e904c93ff85e08a94213b27198f4b8
Alexandre Negrel <alexandre@negrel.dev>
2024-03-18 16:57:33 +0100

    remove dead code

--- box.go
+++
@@ -1,6 +0,0 @@
-package ringo
-
-type box struct {
-	index uint64
-	data  Generic
-}
--- buffer.go
+++ buffer.go
@@ -1,25 +1,7 @@
 package ringo
 
-// Buffer define common methods for ring buffers.
-type Buffer interface {
-	// The capacity of the buffer.
-	Cap() uint32
-
-	// Push the given data to the buffer.
-	// It return whether the push overwrite some
-	// data.
-	Push(Generic) bool
-
-	// Shift data from buffer and return if
-	// the data is valid.
-	Shift() (Generic, bool)
-}
-
-type buffer struct {
-	buffer []Generic
-	head   uint64
-	tail   uint64
-	// NOTE Stored as uint64 to avoid conversion
-	// but will never overflow uint32
-	capacity uint64
+// Box wraps a T to store its index within a ring buffer.
+type box[T any] struct {
+	index uint64
+	data  T
 }
